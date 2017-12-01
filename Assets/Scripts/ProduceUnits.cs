cd3c8bb1009fb04eed249ad3690120359e5656d4
sean brown <seanbrown7935@gmail.com>
2017-11-30 18:36:34 -0700

    Created script to all an object to create units.

---
+++ Assets/Scripts/ProduceUnits.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class ProduceUnits : MonoBehaviour {
+
+	public GameObject unitBlueprint;
+	public float forceMultiple;
+
+	GameObject newUnit;
+	PlayerController player;
+
+	void Start ()
+	{
+		player = FindObjectOfType<PlayerController>();
+	}
+
+	void Update ()
+	{
+		//Produces units over time.
+		if(Input.GetMouseButtonUp(0))
+		{
+			if(player.canSpawn())
+			{
+				newUnit = Instantiate(unitBlueprint, gameObject.transform);
+				Vector3 mousePos = Input.mousePosition;
+				Vector3 nucleusPos = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+
+				Vector3 finalForce = Vector3.Normalize(new Vector3(mousePos.x - nucleusPos.x, mousePos.y - nucleusPos.y, 0.0f)) * forceMultiple;
+
+				newUnit.GetComponent<Rigidbody2D>().velocity = finalForce;
+
+				player.spawnUnit();
+			}
+		}
+	}
+}
