9d738e3c6ec046af45aa10bd395df0ecaaef0428
sean brown <seanbrown7935@gmail.com>
2017-11-30 18:37:46 -0700

    Created text updating scripts.

---
+++ Assets/Scripts/UpdateEnergyText.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateEnergyText : MonoBehaviour {
+
+	PlayerController player;
+
+	Text energyText;
+
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<PlayerController>();
+		energyText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		energyText.text = "Energy = " + player.getEnergy();
+	}
+}
