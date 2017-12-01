f5dd486771837b2e3baa55b393985aaa6ec34804
DESKTOP-535OJ45\seanb <seanbrown7935@gmail.com>
2017-12-01 01:07:58 -0700

    Created script to change object color based on health.

---
+++ Assets/Scripts/HealthIndicator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthIndicator : MonoBehaviour {
+
+    Health healthObject;
+
+	// Use this for initialization
+	void Start () {
+        healthObject = GetComponent<Health>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float healthRatio = healthObject.health / healthObject.maxHealth;
+
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+        renderer.color = new Color(renderer.color.r, renderer.color.b * healthRatio, renderer.color.g * healthRatio);
+	}
+}
