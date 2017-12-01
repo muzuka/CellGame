7ea0a5238ba1424f6e535750687bb0c6ea7f9a05
sean brown <seanbrown7935@gmail.com>
2017-11-30 18:35:00 -0700

    Created script to heal the nucleus.

---
+++ Assets/Scripts/HealsNucleus.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class HealsNucleus : MonoBehaviour {
+
+	public Health nucleusHealth;
+
+	float timeConsumed;
+	const float timeToHeal = 2.0f;
+
+	void Start () {
+		timeConsumed = 0.0f;
+	}
+
+	void Update () {
+		timeConsumed += Time.deltaTime;
+		if(timeConsumed >= timeToHeal)
+		{
+			if(!nucleusHealth.full())
+				nucleusHealth.heal(1);
+
+			timeConsumed = 0.0f;
+		}
+	}
+}
