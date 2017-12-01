539afa5179f58257c3db8d27ee9e0f9224e71766
sean brown <seanbrown7935@gmail.com>
2017-11-30 18:35:51 -0700

    Created player script to track energy.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour, IPlayerMessageTarget {
+
+	int energy;
+	int maxEnergy;
+
+	void Start () {
+		energy = 0;
+		maxEnergy = 0;
+	}
+	
+	public void addEnergy (int amount)
+	{
+		if(energy <= maxEnergy)
+			energy += amount;
+	}
+
+	public void spawnUnit ()
+	{
+		energy--;
+	}
+
+	public int getEnergy ()
+	{
+		return energy;
+	}
+
+	public bool canSpawn ()
+	{
+		return energy > 0;
+	}
+
+	public void vacuoleDestroyed ()
+	{
+		maxEnergy -= 20;
+	}
+
+	public void vacuoleCreated ()
+	{
+		maxEnergy += 20;
+	}
+}
