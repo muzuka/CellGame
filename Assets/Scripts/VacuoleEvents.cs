243326280434bc26bbc841bdcb77f36a5398ca83
sean brown <seanbrown7935@gmail.com>
2017-11-30 18:38:27 -0700

    Created event listening script for vacuoles.

---
+++ Assets/Scripts/VacuoleEvents.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class VacuoleEvents : MonoBehaviour {
+
+	PlayerController player;
+
+	void Start () {
+		player = FindObjectOfType<PlayerController>();
+
+		if(player)
+			ExecuteEvents.Execute<IPlayerMessageTarget>(player.gameObject, null, (x, y)=>x.vacuoleCreated());
+	}
+
+	void OnDestroy () {
+		if(player)
+			ExecuteEvents.Execute<IPlayerMessageTarget>(player.gameObject, null, (x, y)=>x.vacuoleDestroyed());
+	}
+}
