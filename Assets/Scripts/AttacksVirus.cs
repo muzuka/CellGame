8d6371e9bdb4556a5945084d8b77dea78429c50d
sean brown <seanbrown7935@gmail.com>
2017-11-30 18:32:00 -0700

    Created attack script for player units.

---
+++ Assets/Scripts/AttacksVirus.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class AttacksVirus : MonoBehaviour {
+
+	public int attackDamage;
+
+	void OnCollisionEnter2D (Collision2D collision)
+	{
+		if(collision.otherCollider.tag == "Virus")
+		{
+
+		}
+	}
+
+	void OnCollisionStay2D (Collision2D collision)
+	{
+		if(collision.otherCollider.tag == "Virus")
+		{
+			collision.otherCollider.gameObject.GetComponent<Health>().damage(attackDamage);
+		}
+	}
+
+	void OnCollisionExit2D (Collision2D Collision)
+	{
+
+	}
+}
