d5caf9503fcf7cb48d7d19bc2da5777534de7085
sean brown <seanbrown7935@gmail.com>
2017-11-30 18:34:43 -0700

    Created script to give an object the ability to generate energy for the player.

---
+++ Assets/Scripts/GeneratesEnergy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class GeneratesEnergy : MonoBehaviour {
+
+	public int energyProduction;
+	public float timeToEnergy;
+
+	PlayerController player;
+
+	Timer energyTimer;
+
+	void Start ()
+	{
+		energyTimer = new Timer(timeToEnergy);
+		player = FindObjectOfType<PlayerController>();
+	}
+
+	void Update () {
+		energyTimer.update(addEnergy);
+	}
+
+	void addEnergy ()
+	{
+		player.addEnergy(energyProduction);
+	}
+}
