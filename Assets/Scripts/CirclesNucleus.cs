226624b106710c47c487c4f4268b007af0c7ab14
sean brown <seanbrown7935@gmail.com>
2017-11-30 18:32:25 -0700

    Created script to move the centrosome around the nucleus.

---
+++ Assets/Scripts/CirclesNucleus.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class CirclesNucleus : MonoBehaviour {
+
+	public GameObject nucleus;
+	public float radius;
+
+	float x;
+	float y;
+	float angle;
+
+	void Start () 
+	{
+		angle = 0.0f;
+		updatePosition();
+	}
+
+	void Update () 
+	{
+		updatePosition();
+	}
+
+	void updatePosition ()
+	{
+		angle += Time.deltaTime;
+		x = radius * Mathf.Cos(angle) + nucleus.transform.position.x;
+		y = radius * Mathf.Sin(angle) + nucleus.transform.position.y;
+
+		transform.position = new Vector3(x, y, 0.0f);
+	}
+}
