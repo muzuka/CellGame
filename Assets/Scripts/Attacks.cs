103d4f2aa02a905db8f1a4204789c83f123dc347
sean brown <seanbrown7935@gmail.com>
2017-11-30 18:31:34 -0700

    Created script for dealing damage to things that are touching.

---
+++ Assets/Scripts/Attacks.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Attacks relevant things that are touching it.
+/// </summary>
+public class Attacks : MonoBehaviour {
+
+	public float attackTime;
+	public int attackDamage;
+
+	Rigidbody2D rigidBody;
+	LayerMask cellMask;
+	LayerMask virusMask;
+	ContactFilter2D filter;
+
+	Timer timer;
+
+	void Start () {
+		timer = new Timer(attackTime);
+		rigidBody = GetComponent<Rigidbody2D>();
+
+		cellMask = LayerMask.GetMask("Cell", "Nucleus");
+		virusMask = LayerMask.GetMask("Virus");
+
+		filter = new ContactFilter2D();
+
+		if(gameObject.tag == "Virus")
+			filter.SetLayerMask(cellMask);
+		else
+			filter.SetLayerMask(virusMask);
+	}
+
+	void Update () {
+		if(rigidBody.IsTouching(filter))
+			timer.update(attack);
+	}
+
+	void attack ()
+	{
+		Collider2D[] colliders = new Collider2D[10];
+		int contactSize = rigidBody.GetContacts(colliders);
+
+		for(int i = 0; i < contactSize; i++)
+			colliders[0].gameObject.GetComponent<Health>().damage(attackDamage);
+	}
+}
