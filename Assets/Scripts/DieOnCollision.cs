90ac1a2eeee0bcc8de35c7e69bb7c1ab535db5d3
sean brown <seanbrown7935@gmail.com>
2017-11-30 18:32:56 -0700

    Created script for objects to instantly be destroyed on touch.

---
+++ Assets/Scripts/DieOnCollision.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class DieOnCollision : MonoBehaviour {
+
+	void OnCollisionEnter2D (Collision2D other)
+	{
+		Debug.Log("Collision!");
+
+		if(gameObject.tag == "Unit")
+		{
+			if(other.collider.tag == "Virus")
+				Destroy(gameObject);
+		}
+		else if(gameObject.tag == "Virus")
+		{
+			if(other.collider.tag == "Unit")
+				Destroy(gameObject);
+		}
+	}
+}
