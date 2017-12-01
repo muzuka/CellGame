cca02e170f398f1448042b9dcee818c3fcc753e0
DESKTOP-535OJ45\seanb <seanbrown7935@gmail.com>
2017-12-01 01:06:34 -0700

    Renamed vacuoleEvents to vacuoleController.

---
+++ Assets/Scripts/VacuoleController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class VacuoleController : MonoBehaviour {
+
+    public GameObject player;
+
+	void Start () {
+        //player.GetComponent<PlayerController>().vacuoleCreated();
+	}
+
+	void OnDestroy () {
+        player.GetComponent<PlayerController>().vacuoleDestroyed();
+	}
+}
