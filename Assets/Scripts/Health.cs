415ea9749edf220678ff36d8b6ef741dfb0820f2
sean brown <seanbrown7935@gmail.com>
2017-11-30 18:35:12 -0700

    Created health script.

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+	public int maxHealth;
+
+	public int health {
+		get { return health; }
+		set { health = value; }
+	}
+
+	void Start ()
+	{
+		health = maxHealth;
+	}
+
+	void Update () 
+	{
+		if(health <= 0)
+			Destroy(gameObject);
+	}
+
+	public void heal (int amount)
+	{
+		if(health + amount > maxHealth)
+			health = maxHealth;
+		else
+			health += amount;
+	}
+
+	public void damage (int amount)
+	{
+		health -= amount;
+	}
+
+	public bool full ()
+	{
+		return maxHealth == health;
+	}
+}
